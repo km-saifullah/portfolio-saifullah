3237591726b1619ff1c86b141cfad8fe0be09507
km-saifullah <kmsaifullah16@gmail.com>
2024-02-17 21:40:59 +0600

    portfolio dependencies installation done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import MainLayout from "./components/layout/MainLayout";
+import Home from "./pages/home/Home";
+import Contact from "./pages/contact/Contact";
+import NotFound from "./pages/notfound/NotFound";
+
+// All Routes
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </>
+  )
+);
 
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
