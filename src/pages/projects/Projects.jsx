f8378334610bcaf3d64c9a52bca45d88dcfb5e85
km-saifullah <kmsaifullah16@gmail.com>
2024-06-25 09:41:08 +0600

    new backend project added

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -5,7 +5,7 @@ import ProjectCard from "../../components/ProjectCard";
 
 const Projects = () => {
   const frontEnd = projects.slice(0, 6);
-  // const bakendEnd = projects.slice(0, 6);
+  const backend = projects.slice(7, 8);
   const mobile = projects.slice(6, 7);
   return (
     <section className="py-10 md:py-16 lg:py-20">
@@ -35,6 +35,25 @@ const Projects = () => {
           </div>
           <div className="flex flex-col gap-12 px-3">
             {mobile.map((item) => (
+              <ProjectCard
+                keyId={item.id}
+                projectImg={item.projectImage}
+                projectTitle={item.projectName}
+                projectDescription={item.projectDetail}
+                icons={item.projectIcons}
+                projectGithub={item.projectLink}
+                liveSite={item.projectDemo}
+                youtubeLink={item.demoVideo}
+              />
+            ))}
+          </div>
+        </div>
+        <div>
+          <div className="py-2 lg:py-5">
+            <SectionHeading title="Backend Projects" />
+          </div>
+          <div className="flex flex-col gap-12 px-3">
+            {backend.map((item) => (
               <ProjectCard
                 keyId={item.id}
                 projectImg={item.projectImage}
