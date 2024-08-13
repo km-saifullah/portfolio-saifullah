dc77e37eb1a2bf33af75b38b0b8202605b1c4a70
km-saifullah <kmsaifullah16@gmail.com>
2024-08-13 12:22:46 +0600

    npm package code refactore done

--- src/pages/npm/NpmPackage.jsx
+++ src/pages/npm/NpmPackage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import SectionHeading from "../../utils/SectionHeading";
-import packageImg from "../../assets/images/package.png";
-import { Link } from "react-router-dom";
+import Package from "../../utils/Package";
 
 const NpmPackage = () => {
   return (
@@ -10,33 +9,17 @@ const NpmPackage = () => {
         <div className="py-2 lg:py-5">
           <SectionHeading title="NPM Package" />
         </div>
-        <div className="flex items-center justify-between gap-2 flex-wrap-reverse">
-          <div className="w-[500px]">
-            <h1 className="text-xl font-roboto font-bold">quick-app</h1>
-            <p className="w-full xl:w-[585px] text-justify md:text-left pt-2 pb-2 text-base text-textColor font-normal font-openSans leading-[135%]">
-              Starter files and directories for an express application. Using
-              this package you can create a new express app with all necessary
-              files and directories.
-            </p>
-            <img src="https://img.shields.io/npm/dw/quick-app" alt="" />
-            <div className="pt-2">
-              <Link
-                className="text-primary decoration-solid underline"
-                to="https://www.npmjs.com/package/quick-app"
-                target="_blank"
-              >
-                Package Link
-              </Link>
-            </div>
-          </div>
-          <div className="w-[200px] h-[200px]">
-            <img
-              className="h-fll w-full object-cover"
-              src={packageImg}
-              alt=""
-            />
-          </div>
-        </div>
+        <Package
+          packageName="quick-app"
+          packageDetails="Starter files and directories for an express application. Using this
+          package you can create a new express app with all necessary files and directories."
+          packageLink="https://www.npmjs.com/package/quick-app"
+        />
+        <Package
+          packageName="quick-response"
+          packageDetails="API response message for any Node JS application"
+          packageLink="https://www.npmjs.com/package/quick-response"
+        />
       </div>
     </section>
   );
---
+++ src/utils/Package.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import packageImg from "../assets/images/package.png";
+import { Link } from "react-router-dom";
+
+const Package = ({ packageName, packageDetails, packageLink }) => {
+  let imgSrc = `https://img.shields.io/npm/dw/${packageName}`;
+  return (
+    <div className="flex items-center justify-between gap-2 flex-wrap-reverse">
+      <div className="w-[500px]">
+        <h1 className="text-xl font-roboto font-bold">quick-app</h1>
+        <p className="w-full xl:w-[585px] text-justify md:text-left pt-2 pb-2 text-base text-textColor font-normal font-openSans leading-[135%]">
+          {packageDetails}
+        </p>
+        <img src={imgSrc} alt={packageName} />
+        <div className="pt-2">
+          <Link
+            className="text-primary decoration-solid underline"
+            to={packageLink}
+            target="_blank"
+          >
+            Package Link
+          </Link>
+        </div>
+      </div>
+      <div className="w-[200px] h-[200px]">
+        <img className="h-fll w-full object-cover" src={packageImg} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Package;
